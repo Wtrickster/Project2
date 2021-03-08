3e20fe4d01ec9cfce40d47603b00dcc47637ad26
Austin Ruhf <austin.ruhf@gmail.com>
2021-03-07 19:47:42 -0500

    changed walk time

--- db/seed.sql
+++ db/seed.sql
@@ -4,5 +4,5 @@ INSERT INTO  dogs (dog_name, owner_name, address, phone_number, long_walk, walk_
     ("Winky", "Cookie Fleck", "7412 Bohemia St. Palm Bay, FL 32907", "606 895 5734", false, 130000, null),
     ("Beatrice", "Hamilton Swan", "20 Pendergast St. Highland Park, IL 60035", "452 503 9696", true, 154500, "Beatrice has to meditate at the statue in Abbie Park midway through her walk. Make sure no other dogs look at her while she does this."),
     ("Hubert", "Harlan Pepper", "60 Elmwood Street Clayton, NC 27520", "958 361 7968", false, 113000, "Hubert is a slow old boy but he still pulls hard. Make sure he doesn't eat anything."),
-    ("Rhapsody in White", "Sherri Ann", "77 Aspen St. Hazleton, PA 18201", "245 594 4308", true, 163000, "Please make sure Rhapsody gets at least 3 kilometers before you bring her back home."),
+    ("Rhapsody in White", "Sherri Ann", "77 Aspen St. Hazleton, PA 18201", "245 594 4308", true, 110000, "Please make sure Rhapsody gets at least 3 kilometers before you bring her back home."),
     ("Miss Agnes", "Stefan Vanderhoof", "474 Summer St. Woonsocket, RI 02895", "648 715 4511", false, 123000, null)
