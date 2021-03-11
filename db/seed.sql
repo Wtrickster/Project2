76b55b931cf873acc6101842046d6038a42cb18e
Austin Ruhf <austin.ruhf@gmail.com>
2021-03-10 19:20:11 -0500

    fixed DB error

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ USE  hg1sixrm9gn0c7w7;
 
 INSERT INTO  dogs (dog_name, breed, owner_name, address, phone_number, extra_notes, long_walk, walked_today, walking, walk_time, walk_number) VALUES
     ("Winky", "Norwich Terrier","Cookie Fleck", "7412 Bohemia St. Palm Bay, FL 32907", "606 895 5734",null, 0, 0, 0, 1, 2),
-    ("Beatrice", "Weimaraner" "Hamilton Swan", "20 Pendergast St. Highland Park, IL 60035", "452 503 9696", "Beatrice has to meditate at the statue in Abbie Park midway through her walk. Make sure no other dogs look at her while she does this.", 1, 0, 0, 3, 0),
+    ("Beatrice", "Weimaraner", "Hamilton Swan", "20 Pendergast St. Highland Park, IL 60035", "452 503 9696", "Beatrice has to meditate at the statue in Abbie Park midway through her walk. Make sure no other dogs look at her while she does this.", 1, 0, 0, 3, 0),
     ("Hubert", "Bloodhound", "Harlan Pepper", "60 Elmwood Street Clayton, NC 27520", "958 361 7968", "Hubert is a slow old boy but he still pulls hard. Make sure he doesn't eat anything.", 0, 0, 0, 1, 3),
     ("Rhapsody in White", "Standard Poodle", "Sherri Ann", "77 Aspen St. Hazleton, PA 18201", "245 594 4308", "Please make sure Rhapsody gets at least 3 kilometers before you bring her back home.", 1, 0, 0, 1, 7),
     ("Miss Agnes", "Shih Tzu","Stefan Vanderhoof", "474 Summer St. Woonsocket, RI 02895", "648 715 4511", null, 0, 0, 0, 2, 6);
