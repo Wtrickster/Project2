2838575a8117689241f54cb3bdf0bf4cccc8a6cc
Joseph <JSPenland97@gmai.com>
2021-03-15 18:20:40 -0400

    js

---
+++ config/orm.js
@@ -0,0 +1,103 @@
+// Import MySQL connection.
+const connection = require("./connection.js");
+
+// Helper function for SQL syntax to add question marks (?, ?, ?) in query
+const printQuestionMarks = num => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = ob => {
+  const arr = [];
+
+  // Loop through the keys and push the key/value as a string int arr
+  for (const key in ob) {
+    let value = ob[key];
+    // Check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // If string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = `'${value}'`;
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(`${key}=${value}`);
+    }
+  }
+
+  // Translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
+// Object for all our SQL statement functions.
+const orm = {
+  all(tableInput, cb) {
+    const queryString = `SELECT * FROM ${tableInput};`;
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  create(table, cols, vals, cb) {
+    let queryString = `INSERT INTO ${table}`;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // An example of objColVals would be {name: panther, sleepy: true}
+  update(table, objColVals, condition, cb) {
+    let queryString = `UPDATE ${table}`;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  delete(table, condition, cb) {
+    let queryString = `DELETE FROM ${table}`;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, (err, result) => {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  }
+};
+
+// Export the orm object for the model (cat.js).
+module.exports = orm;
--- public/js/script.js
+++ public/js/script.js
@@ -1,99 +1,150 @@
-$(document).ready(function() {
+/* eslint-disable quotes */
+$(document).ready(() => {
+  const express = require("express");
+  const exphbs = require("express-handlebars");
+  const mysql = require("mysql");
 
-    const express = require('express');
-    const exphbs = require('express-handlebars');
-    const mysql = require('mysql');
+  const app = express();
 
-    const app = express();
+  $(".addForm").hide();
 
-    $(".addForm").hide();
+  function addFormDisplay(e) {
+    e.preventDefault();
+    $(".addForm").show();
+  }
 
-    function addFormDisplay(e) {
-        e.preventDefault();
-        $(".addForm").show();
+  $(".addButton").click(addFormDisplay);
+
+  const todayDisplay = moment().format("ddd[ - ] MMMM Do[,] YYYY");
+  console.log(todayDisplay);
+  $("#currentDay").text(todayDisplay);
+
+  const currentTime = parseInt(moment().format("hh"));
+
+  // Set the port of our application
+  // process.env.PORT lets the port be set by Heroku
+  const PORT = process.env.PORT || 8080;
+
+  // Sets up the Express app to handle data parsing
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+  app.set("view engine", "handlebars");
+
+  // const connection = mysql.createConnection({
+  //     host: 'localhost',
+  //     port: 3306,
+  //     user: 'root',
+  //     password: '',
+  //     database: 'hg1sixrm9gn0c7w7',
+  // });
+
+  // connection.connect((err) => {
+  //     if (err) {
+  //         console.error(`error connecting: ${err.stack}`);
+  //         return;
+  //     }
+
+  //     console.log(`connected as id ${connection.threadId}`);
+  // });
+
+  //add a dog
+  $("input#newDog").on("click", () => {
+    const dog_Name = $("input#dog_Name").val();
+    if ($.trim(dog_name) !== " ") {
+      $.post("ajax/dog_Name.php", { dog_Name: dog_Name });
     }
+    const breed = $("input#breed").val();
+    if ($.trim(breed) !== " ") {
+      $.post("ajax/breed.php", { breed: breed });
+    }
+    const owner_Name = $("input#owner_Name").val();
+    if ($.trim(owner_Name) !== " ") {
+      $.post("ajax/owner_Name.php", { owner_Name: owner_Name });
+    }
+    const dog_info = $("input#dog_info").val();
+    $.post("ajax/dog_info.php", { dog_info: dog_info });
+    if ($("#long_walk").attr("checked")) {
+      $.post("ajax/long_walk.php", { long_walk: true });
+    } else {
+      $.post("ajax/long_walk.php", { long_walk: false });
+    }
+  });
 
-    $(".addButton").click(addFormDisplay)
+  //set up listener for dog button
+  if (changeDevourBtns) {
+    changeDevourBtns.forEach(button => {
+      button.addEventListener("click", e => {
+        //grabs the id of the element that goes by the name, "id"
+        const id = e.target.getAttribute("data-id");
+        const newDevour = e.target.getAttribute("data-newDevour");
 
-    var todayDisplay = moment().format('ddd[ - ] MMMM Do[,] YYYY');
-    console.log(todayDisplay)
-    $("#currentDay").text(todayDisplay);
-
-    var currentTime = parseInt(moment().format('hh'));
-
-    // Set the port of our application
-    // process.env.PORT lets the port be set by Heroku
-    const PORT = process.env.PORT || 8080;
-
-    // Sets up the Express app to handle data parsing
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-
-    app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-    app.set('view engine', 'handlebars');
-
-    // const connection = mysql.createConnection({
-    //     host: 'localhost',
-    //     port: 3306,
-    //     user: 'root',
-    //     password: '',
-    //     database: 'hg1sixrm9gn0c7w7',
-    // });
-
-    // connection.connect((err) => {
-    //     if (err) {
-    //         console.error(`error connecting: ${err.stack}`);
-    //         return;
-    //     }
-
-    //     console.log(`connected as id ${connection.threadId}`);
-    // });
-
-    //add a dog
-    $('input#newDog').on('click', function() {
-        var dog_Name = $('input#dog_Name').val();
-        if ($.trim(dog_name) != ' ') {
-            $.post('ajax/dog_Name.php', { dog_Name: dog_Name })
+        const newDevourState = {
+          devoured: newDevour
         };
-        var breed = $('input#breed').val();
-        if ($.trim(breed) != ' ') {
-            $.post('ajax/breed.php', { breed: breed })
-        };
-        var owner_Name = $('input#owner_Name').val();
-        if ($.trim(owner_Name) != ' ') {
-            $.post('ajax/owner_Name.php', { owner_Name: owner_Name })
-        };
-        var dog_info = $('input#dog_info').val();
-        $.post('ajax/dog_info.php', { dog_info: dog_info })
-        if ($('#long_walk').attr('checked')) {
-            $.post('ajax/long_walk.php', { long_walk: true })
-        } else {
-            $.post('ajax/long_walk.php', { long_walk: false })
-        };
+
+        fetch(`/api/burgers/${id}`, {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+
+          //make sure to serialize the JSON body
+          body: JSON.stringify(newDevourState)
+        }).then(response => {
+          //check that the response is all good
+          //reload the page so the user can see the new quote
+          if (response.ok) {
+            console.log(`changed devour to: ${newDevour}`);
+            location.reload("/");
+          } else {
+            alert("something went wrong!");
+          }
+        });
+      });
     });
+  }
 
-    //needs to be walked
-    $('input#newDog').on('click', function() {
+  //CREATE
+  const createBurgerBtn = document.getElementById("create-form");
 
+  if (createBurgerBtn) {
+    createBurgerBtn.addEventListener("submit", e => {
+      e.preventDefault();
 
+      //grabs the value of the textarea that goes by the name, "quote"
+      const newBurger = {
+        name: document.getElementById("ba").value.trim()
+        //sleepy: document.getElementById('sleepy').checked,
+      };
+
+      //send POST request to create a new quote
+      fetch("/api/burgers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+
+        //make sure to serialize the JSON body
+        body: JSON.stringify(newBurger)
+      }).then(() => {
+        //empty the form
+        document.getElementById("ba").value = "";
+
+        //reload the page so the user can see the new quote
+        console.log("Created a new burger!");
+        location.reload();
+      });
     });
-
-    //being walked
-    $('input#newDog').on('click', function() {
-
-
-    });
-
-    //finished being walked
-    $('input#newDog').on('click', function() {
-
-
-    });
-
-    // Start our server so that it can begin listening to client requests.
-    // Log (server-side) when our server has started
-    app.listen(PORT, () =>
-        console.log(`Server listening on: http://localhost:${PORT}`)
-    );
-
+  }
+  //start our server so that it can begin listening to client requests.
+  //log (server-side) when our server has started
+  app.listen(
+    PORT,
+    () => console.log(`Server listening on: http://localhost:${PORT}`)
+    //eslint-disable-next-line indent
+  );
 });
